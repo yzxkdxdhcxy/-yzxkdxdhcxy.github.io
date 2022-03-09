c8d3301f7aa5206f12fd06c7ce2e283d65f1e6a3
yzxkdxdhcxy <101191459+yzxkdxdhcxy@users.noreply.github.com>
2022-03-09 22:30:42 +0800

    Add files via upload

--- thumbnail.js
+++ thumbnail.js
@@ -1,5 +1,3 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    <img src="https://game.gtimg.cn/images/lol/act/img/skin/big157036.jpg">
-    class="small"
-
+     class="small"
 });
